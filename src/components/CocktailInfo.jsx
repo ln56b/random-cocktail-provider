bbdcc5412b850c52eb9d7f7c0bbb8998b722b698
Hélène Baudrand <baudrand.helene@gmail.com>
2019-11-03 10:33:20 +0100

    final incl comments

--- src/components/CocktailInfo.jsx
+++ src/components/CocktailInfo.jsx
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 
+//7. Initialise SL info component
+//9. Catch infos as props and destructurate it via a new const.
+//10. Get destructurated props in the div.
 const CocktailInfo = ({ infos }) => {
   const {strDrink, strDrinkThumb} = infos;
   return(
     <Fragment>
-      <img src={strDrinkThumb}></img>
+      <img src={strDrinkThumb} alt={strDrink}></img>
       <h1>{strDrink}</h1>
     </Fragment>
   )
--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -2,20 +2,25 @@ import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import CocktailInfo from './CocktailInfo';
 
+//1. cccs create Class Component
 class Cocktails extends Component {
   constructor(props) {
     super(props);
+    //2. initialise state
     this.state = { 
       randomCocktail: null,
     };
+    //6. Bind
     this.fetchRandomCocktail = this.fetchRandomCocktail.bind(this);
   }
+  //12. Add DidMount to avoid need of a button
   componentDidMount() {
     this.fetchRandomCocktail();
   }
-
+  //4. Initialise method to get it
   fetchRandomCocktail() {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    //5. Define axios actions and setState
     axios.get(url)
       .then(response => response.data)
       .then(data => data.drinks[0])
@@ -28,6 +33,10 @@ class Cocktails extends Component {
   
   render() {
     return (
+      /*3. Initialise action element in return*/
+      /*8. create const infos as state in Component*/
+      /*11 check if this.state is true && call component*/
+
       <Fragment>
         <button onClick={this.fetchRandomCocktail}>Another cocktail</button>
         {this.state.randomCocktail && <CocktailInfo infos= {this.state.randomCocktail} />}      
